cdef63b9231fa23ecac1fe421faf1a4d52aaa821
Hasibul Islam Nirob <hasibolislamnirob@gmail.com>
2024-11-07 00:48:32 +0600

    task 2, initiate

---
+++ task-2/server.js
@@ -0,0 +1,88 @@
+
+const express  = require('express');
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
+
+const {Server} = require('socket.io');
+const io = new Server(expressServer);
+
+
+io.on("connection", function(socket){
+    console.log('New connected');
+    socket.on("disconnect", function(){
+        console.log('Disconnected');
+    });
+
+    /*
+    // Predefine Event
+    setInterval(function(){
+        let date = new Date();
+        let time = date.getTime();
+        socket.send(time);
+    },1000)
+    
+
+    // Coustom Event
+    setInterval(function(){
+        let date = new Date();
+        let time = date.getTime();
+        socket.emit('sendMsgEvent', time);
+    },1000)
+
+    // Client to Server
+    socket.on('message', function(msg){
+        console.log(msg);
+        
+    })
+    
+    socket.on('sendMsgEvent', function(msg){
+        console.log(msg);
+        
+    })
+        */
+
+    // Broadcasting
+    io.sockets.emit('msgBroadcasting', 'Hello Everybody')
+
+});
+
+
+let adminRoom = io.of('/admin');
+adminRoom.on("connection", function(socket){
+    console.log('New admin connected');
+    socket.on("disconnect", function(){
+        console.log('Admin disconnected');
+    });
+
+    adminRoom.emit('msgBroadcasting', 'Welcome to Admin Room')
+})
+
+let userRoom = io.of('/user');
+userRoom.on("connection", function(socket){
+    console.log('New user connected');
+    socket.on("disconnect", function(){
+        console.log('User disconnected');
+    });
+
+    userRoom.emit('msgBroadcasting', 'Welcome to User Room')
+})
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname +'/index.html');
+});
+
+
+app.get('/admin', function(req, res){
+    res.sendFile(__dirname +'/admin.html');
+});
+
+app.get('/user', function(req, res){
+    res.sendFile(__dirname +'/user.html');
+});
+
+expressServer.listen(4000, function(){
+    console.log('Server run @4000');
+    
+})
